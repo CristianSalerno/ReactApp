ae0954ce602e716f53d43661c447cb02f8ef3181
Cristian Salerno <cristiansalerno18@gmail.com>
2019-12-30 22:46:51 +0100

    component seasons

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+
+
+    //Way 1 to initialize state : 
+    constructor(props) {
+        super(props);
+        console.log(props); s
+    }
+
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
